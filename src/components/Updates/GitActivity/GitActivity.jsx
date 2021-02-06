2d9ab8954f1b7ba97adb7aa0847533f41e96a52c
Vikas Movva <vikas.s.movva@gmail.com>
2021-02-05 23:15:53 -0500

    added event icons for github events

--- src/components/Updates/GitActivity/GitActivity.jsx
+++ src/components/Updates/GitActivity/GitActivity.jsx
@@ -1,47 +1,76 @@
-import React from 'react'
 import {
-    Body,
-    Header,
-    GithubLogo,
-    ProfileName,
-    ProfilePic,
-    ProfilelLink,
-    ProfilelNameLink,
-    GitActivityContainer,
-    Events,
-    EventsContainer,
-} from '../UpdatesElements.js'
+  Body,
+  Header,
+  GithubLogo,
+  ProfileName,
+  ProfilePic,
+  ProfilelLink,
+  ProfilelNameLink,
+  GitActivityContainer,
+  Event,
+  EventContainer,
+  EventIcon,
+} from "../UpdatesElements.js";
+
+import {
+  GitForkIcon,
+  GitCommitIcon,
+  GitPullRequestIcon,
+  GitBranchIcon,
+  GitMergeIcon,
+  RepoIcon,
+  StarFillIcon,
+} from "@primer/octicons-react";
 const GitActivity = (props) => {
-    return (
-        <GitActivityContainer>
-            <Header>
-                <ProfilelLink href={props.profileUrl} target="_blank">
-                    <ProfilePic src={props.profilePic} />
-                </ProfilelLink>
+  return (
+    <GitActivityContainer>
+      <Header>
+        <ProfilelLink href={props.profileUrl} target="_blank">
+          <ProfilePic src={props.profilePic} />
+        </ProfilelLink>
 
-                <ProfileName>
-                    <ProfilelNameLink href={props.profileUrl} target="_blank">
-                        {props.profileName}
-                    </ProfilelNameLink>
-                </ProfileName>
-                
-                <GithubLogo/>
-            </Header>
-            <Body>
-                <EventsContainer>
-                    {/* {props.events.map((item, index) => {
-                        return(<Events></Events>)
-                    })} */}
-                </EventsContainer>
-            </Body>
-        </GitActivityContainer>
-    )
-}
+        <ProfileName>
+          <ProfilelNameLink href={props.profileUrl} target="_blank">
+            {props.profileName}
+          </ProfilelNameLink>
+        </ProfileName>
 
-// props.events.map((item, index) => {
-//                             return (<Events key={index}>
-//                                 {item.actor.id}
-                                
-//                             </Events>);
-//                         })
-export default GitActivity
+        <GithubLogo />
+      </Header>
+      <Body>
+        <EventContainer>
+          {props.events.map((item, index) => {
+            return (
+              <Event key={index}>
+                <EventIcon>
+                  {item.type === "PushEvent" ? (
+                    <GitCommitIcon />
+                  ) : item.type === "PullRequestEvent" ? (
+                    item.payload.action === "opened" ? (
+                      <GitPullRequestIcon />
+                    ) : item.payload.action === "closed" &&
+                      item.payload.pull_request.merged === true ? (
+                      <GitMergeIcon />
+                    ) : null
+                  ) : item.type === "ForkEvent" ? (
+                    <GitForkIcon />
+                  ) : item.type === "CreateEvent" ? (
+                    item.payload.ref_type === "branch" ? (
+                      <GitBranchIcon />
+                    ) : item.payload.ref_type === "repository" ? (
+                      <RepoIcon />
+                    ) : null
+                  ) : item.type === "WatchEvent" ? (
+                    <StarFillIcon />
+                  ) : null}
+                </EventIcon>
+              </Event>
+            );
+          })}
+        </EventContainer>
+      </Body>
+    </GitActivityContainer>
+  );
+};
+
+export default GitActivity;
--- src/components/Updates/UpdatesElements.js
+++ src/components/Updates/UpdatesElements.js
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
-import {FaGithub} from 'react-icons/fa'
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
 
 //Constants
-const primary = '#202034'
-const secondary = '#E59A6F'
+const primary = "#202034";
+const secondary = "#E59A6F";
 
 export const UpdatesContainer = styled.div`
   display: grid;
-  grid-template-columns:1fr;
+  grid-template-columns: 1fr;
   grid-template-rows: 1.5fr 8fr;
-  background: #E59A6F;
+  background: #e59a6f;
   width: 100%;
   justify-content: center;
   height: ${window.innerHeight - 80}px;
-`
+`;
 
 export const UpdatesTitle = styled.h1`
   color: ${primary};
@@ -22,93 +22,102 @@ export const UpdatesTitle = styled.h1`
   height: 5%;
   margin-top: 1%;
   margin-bottom: 2%;
-`
+`;
 
 //Github activity components
 export const GitActivityContainer = styled.div`
   width: 70%;
   justify-self: center;
-`
+`;
 
-  //Header components
-  export const Header = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100px;
-    background: ${primary};
-    place-items: center;
-    border-radius: 8px; 
-    border-bottom: 2px solid #505065;
-    position: relative;
-    z-index: 1;
-  `
+//Header components
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100px;
+  background: ${primary};
+  place-items: center;
+  border-radius: 8px;
+  position: relative;
+  z-index: 1;
+`;
 
-  export const ProfileName = styled.h2`
-    margin-left: 2%;
-    padding: 2%;
-    border-radius: 10px;
-    transition: all 0.2s ease;
-    text-decoration: none;
+export const ProfileName = styled.h2`
+  margin-left: 2%;
+  padding: 2%;
+  border-radius: 10px;
+  transition: all 0.2s ease;
+  text-decoration: none;
 
-    &:hover{
-      background: #505065;
-    }
-  `
-  export const ProfilelNameLink = styled.a`
-    text-decoration: none;
-    color: ${secondary};
-  `
+  &:hover {
+    background: #505065;
+  }
+`;
+export const ProfilelNameLink = styled.a`
+  text-decoration: none;
+  color: ${secondary};
+`;
 
-  export const GithubLogo = styled(FaGithub)`
-    width: auto;
-    height: 90%;
-    margin-left: auto;
-    margin-right: 2%;
-    border-radius: 100%;
-    color: ${secondary};
-  `
-  export const ProfilePic = styled.img`
-    width: 80px;
-    height: 80px;
+export const GithubLogo = styled(FaGithub)`
+  width: auto;
+  height: 90%;
+  margin-left: auto;
+  margin-right: 2%;
+  border-radius: 100%;
+  color: ${secondary};
+`;
+export const ProfilePic = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 5px;
+`;
+
+export const ProfilelLink = styled.a`
+  width: 80px;
+  height: 80px;
+  margin-left: 1%;
+`;
+
+//Body components
+export const Body = styled.div`
+  width: 100%;
+  height: 500px;
+  background: ${primary};
+  position: relative;
+  top: -5px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+    background: #000015;
     border-radius: 5px;
-  `
+  }
 
-  export const ProfilelLink = styled.a`
-    width: 80px;
-    height: 80px;
-    margin-left: 1%;
-    
-  `
+  &::-webkit-scrollbar-thumb {
+    background: #d58a5f;
+    border-radius: 5px;
+    border: 1px solid ${primary};
+  }
 
-  //Body components
-  export const Body = styled.div`
-    width: 100%;
-    height: 500px;
-    background: ${primary};
-    position: relative;
-    top: -5px;
-    overflow-y: scroll;
+  &::-webkit-scrollbar-thumb:hover {
+    background: #d58a5f;
+  }
+`;
 
+export const EventContainer = styled.ul`
+  margin-top: 5px;
+  height: 100%;
+`;
 
-    &::-webkit-scrollbar{
-      width: 10px;
-      background: #000015;
-      border-radius: 5px;
-    }
+export const Event = styled.li`
+  height: 20%;
+  display: flex;
+  align-items: center;
+  background: #303045;
+`;
 
-    &::-webkit-scrollbar-thumb {
-      background: ${secondary};
-    }
-
-    &::-webkit-scrollbar-thumb:hover{
-      background: #d58A5F;
-    }
-  `
-
-  export const EventsContainer = styled.ul`
-    
-  `
-
-  export const Events = styled.li`
-    
-  `
+export const EventIcon = styled.div`
+  margin-left: 2%;
+  transform: scale(2);
+  color: #c57a6f;
+`;
