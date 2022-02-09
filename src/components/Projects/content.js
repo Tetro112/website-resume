cc588a3875970e6a34fc0258c26aca1b75be22a9
Vikas Movva <vikas.s.movva@gmail.com>
2022-02-08 19:42:36 -0500

    Added Reddit Slideshow to projects contents

--- src/components/Projects/content.js
+++ src/components/Projects/content.js
@@ -1,11 +1,19 @@
 import { FaAngular, FaReact, FaVuejs } from "react-icons/fa";
+import { RiFlutterFill } from "react-icons/ri";
 
 export const projectsObjects = [
   {
-    title: "Github Activity feed component for React",
+    title: "React Github Feed",
     url: "#",
     description:
-      "A simple and easy to set up activity feed component for React",
+      "A simple and easy to set up activity feed component for React.",
     tech: [<FaReact />],
+  },
+  {
+    title: "Reddit Slideshow",
+    url: "#",
+    description:
+      "A Slideshow app that displays pictures from your favorite subreddits.",
+    tech: [<RiFlutterFill />],
   },
 ];
